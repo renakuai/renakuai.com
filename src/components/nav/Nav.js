0bc3c70f6cf914e9954480857c143f4b2ce7af75
Rena <rena.kuai@gmail.com>
2022-10-11 20:33:23 -0500

    Finished nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './_nav.scss';
 
 function Nav() {
   return (
-    <nav>
-      This is a nav
+    <nav className="Top">
+      <div className="Left">
+        <h5><a href="https://www.renakuai.com">Rena Kuai</a></h5>
+      </div>
+
+      <div className="Right">
+        <ul>
+          <li>Projects (WIP)</li>
+          <li>Writing (WIP)</li>
+        </ul>
+      </div>
     </nav>
   );
 }
