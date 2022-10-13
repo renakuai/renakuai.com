8839ef93d04f4b52a3902aba5093499aff7f82b1
Rena <rena.kuai@gmail.com>
2022-10-13 11:22:23 -0500

    Added post

--- src/components/home/recents/RecentWriting.js
+++ src/components/home/recents/RecentWriting.js
@@ -7,15 +7,15 @@ function RecentProjects() {
     <Recents
       title="Recent Writing"
       route="/writing"
-      link="https://www.figma.com/community/file/1158568599671392782"
       linkName="View more writing"
     >
       <Card
         title="A Primer on Design Tokens"
         date="Oct 2022"
-
+        link="https://medium.com/@renakuai/a-primer-on-design-tokens-f7dd432f06ae"
       >
-        Placeholder for something coming soon.
+        What is a Design Token?
+        Design tokens refer to special variables that store design decisions. From a technical standpoint, they are pieces of information that are associated, at a minimum, with a name-value pair.
 
       </Card>
     </Recents>
--- src/components/writing/Writing.js
+++ src/components/writing/Writing.js
@@ -12,9 +12,13 @@ function Writing() {
         <Card
           title="A Primer on Design Tokens"
           date="Oct 2022"
-          link="https://medium.com/@renakuai"
+          link="https://medium.com/@renakuai/a-primer-on-design-tokens-f7dd432f06ae"
         >
-          Placeholder for something coming soon.
+          What is a Design Token?
+          Design tokens refer to special variables that store design decisions. From a technical standpoint, they are pieces of information that are associated, at a minimum, with a name-value pair.
+          Design tokens act as foundational building blocks and the sources of truth for design systems and can help designers and engineers build more efficiently and consistently. Here are some examples of design tokens used in my personal website:
+
+
         </Card>
 
       </section>
